f7fd8678a93e914d6b5757b735ca4d5502fa624d
Thiago Oliveira Nunes <Thiagooliveiranunes98@gmail.com>
2026-05-24 17:42:57 -0300

    fix vite allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: "0.0.0.0"
+  },
+
   preview: {
+    host: "0.0.0.0",
     allowedHosts: [
-      "inventory-management-frontend-production-975d.up.railway.app"
+      "inventory-management-frontend-production-4499.up.railway.app"
     ]
   }
 })
